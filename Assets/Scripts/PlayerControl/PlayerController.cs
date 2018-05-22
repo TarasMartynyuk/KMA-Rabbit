47c87ec0d37d217e0509757679b8368cc63bf35b
TarasMartynyuk <tarasmartynyuk22@gmail.com>
2018-05-22 14:19:20 +0300

    horiz movement component and player controller monobehaviour

---
+++ Assets/Scripts/PlayerControl/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+    // per second
+    [SerializeField] 
+    float _speed;
+
+    PlayerMovement _playerMovement;
+
+    #region Unity methods
+    void Start () 
+	{
+	    InitComponents();
+	}
+	
+	void Update () 
+	{
+	    UpdateComponents();
+	}
+
+    void FixedUpdate()
+    {
+        FixUpdateComponents();
+    }
+    #endregion
+
+    #region helpers
+    void InitComponents()
+    {
+        var rb = GetComponent<Rigidbody2D>();
+        _playerMovement = new PlayerMovement(rb, transform, _speed);
+
+    }
+
+    void UpdateComponents()
+    {
+        _playerMovement.Update();
+    }
+
+    void FixUpdateComponents()
+    {
+        _playerMovement.FixedUpdate();
+    }
+    #endregion
+}
