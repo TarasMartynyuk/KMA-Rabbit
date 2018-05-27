1f67e2fe98d2a127e1bb94b1dfaf6aa9a80d39d8
TarasMartynyuk <tarasmartynyuk22@gmail.com>
2018-05-28 01:15:55 +0300

    horiz tiling background base monobehaviour

---
+++ Assets/Scripts/Background-Camera/MonoBehaviours/HorizontalTilingBackgroundEditorInputProvider.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Assets.Scripts.Background_Camera.MonoBehaviours
+{
+    abstract class HorizontalTilingBackgroundEditorInputProvider : MonoBehaviour
+    {
+        [SerializeField]
+        protected Camera _camera;
+        // the backgrounds must be either a single sprite gameobjects,
+        // or a gameobject that serves as agroup for sprites, and has a CompositeSprite component
+        [SerializeField]
+        protected GameObject _background;
+        [SerializeField]
+        protected GameObject _backgroundCopy;
+
+        protected HorizontalTilingBackground _tilingBackground;
+
+        protected virtual void Start()
+        {
+            var sprite = GetBoundingSpriteRenderer(_background);
+            Assert.IsNotNull(sprite, $"{nameof(_background)} has neither a CompositeSpriteComponent, nor a SpriteRenderer component");
+
+            var spriteCopy = GetBoundingSpriteRenderer(_backgroundCopy);
+            Assert.IsNotNull(spriteCopy, $"{nameof(_backgroundCopy)} has neither a CompositeSpriteComponent, nor a SpriteRenderer component");
+
+            _tilingBackground = new HorizontalTilingBackground(
+                _camera, sprite, spriteCopy);
+        }
+
+        /// <summary>
+        /// returns the bounding sprite, if argument is a composite sprite GO,
+        /// or the sprite rendere of an argument, if it is a single sprite
+        /// returns null if it neither
+        /// </summary>
+        /// <returns></returns>
+        static SpriteRenderer GetBoundingSpriteRenderer(GameObject background)
+        {
+            var compositeSprite = background.GetComponent<CompositeSprite>();
+
+            if(compositeSprite != null) 
+            { return compositeSprite.BoundingSprite; }
+
+            return background.GetComponent<SpriteRenderer>();
+        }
+    }
+}
